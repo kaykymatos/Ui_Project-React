3c8013e2624dadcd68245aeaaa3cf151abb042c3
Kayky Matos <kayky7277@gmail.com>
2022-11-05 16:40:23 -0300

    [1-updates_projeto] React-Router-Dom configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
